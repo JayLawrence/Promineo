1a32ed3e324a828e321e460f99428675fd0b59d8
JayLawrence <jay.lawrence.11@gmail.com>
2022-09-06 10:50:12 -0700

    commit on September 6th, beginning of Spring / REST API project

---
+++ Week_13 Spring_REST_project/jeep-sales/src/main/java/com/promineotech/jeep/controller/DefaultJeepSalesController.java
@@ -0,0 +1,16 @@
+package com.promineotech.jeep.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import com.promineotech.jeep.entity.Jeep;
+
+@RestController
+public class DefaultJeepSalesController implements JeepSalesController {
+
+  @Override
+  public List<Jeep> fetchJeeps(String model, String trim) {
+    
+    return null;
+  }
+
+}
